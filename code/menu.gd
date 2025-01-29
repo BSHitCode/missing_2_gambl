f2a0e9e49bacabbbfb78a487169c52b2c12c6452
FelixWoess <felix.woess@aon.at>
2025-01-29 21:26:48 +0100

    UI improvement
    Added Style to buttons

--- code/menu.gd
+++ code/menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$VBoxContainer/Start.grab_focus()
+	$PanelContainer/MarginContainer/VBoxContainer/MarginContainer/Start.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -11,8 +11,10 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/GamblTest.tscn")
+	get_tree().change_scene_to_file("res://scenes/GamblSelect.tscn")
 
-
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Options.tscn")
+	
 func _on_quit_pressed() -> void:
 	get_tree().quit(0)
